a1efeeab1b4f3f63be9f09d6f82f74276bf903b4
Allison Bojarski <alizinha@gmail.com>
2015-05-31 16:31:35 -0400

    worked on the first feature

--- MemeifyMe/app/src/main/java/accesscode/c4q/nyc/memeifyme/MainActivity.java
+++ MemeifyMe/app/src/main/java/accesscode/c4q/nyc/memeifyme/MainActivity.java
@@ -1,15 +1,35 @@
 package accesscode.c4q.nyc.memeifyme;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button camera_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        camera_button = (Button) findViewById(R.id.camera_button);
+
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Camera.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
