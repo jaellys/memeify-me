637458f692b285e01fe98a05dfff6965ddff2f30
hyunj0 <hyunj0@hotmail.com>
2015-05-31 13:00:57 -0400

    Connect to Camera & Gallery; made rough meme editor

--- MemeifyMe/app/src/main/java/accesscode/c4q/nyc/memeifyme/MainActivity.java
+++ MemeifyMe/app/src/main/java/accesscode/c4q/nyc/memeifyme/MainActivity.java
@@ -1,15 +1,42 @@
 package accesscode.c4q.nyc.memeifyme;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button takePicture, getPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        takePicture = (Button) findViewById(R.id.takePicture);
+        getPicture = (Button) findViewById(R.id.getPicture);
+
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(takePicture);
+            }
+        });
+
+        getPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent getPicture = new Intent(Intent.ACTION_GET_CONTENT);
+                getPicture.setType("image/*");
+                startActivity(getPicture);
+            }
+        });
+
+        Intent addMeme = new Intent(getApplicationContext(), AddMemeCaption.class);
+        startActivity(addMeme);
     }
 }
